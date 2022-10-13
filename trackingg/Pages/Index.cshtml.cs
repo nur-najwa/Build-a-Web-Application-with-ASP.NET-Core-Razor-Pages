3ff3e429bb458c14d8800a946c6c8487d4458238
Najwa <najwa@iris.com.my>
2022-10-13 10:20:12 +0800

    first commit

---
+++ trackingg/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using trackingg.Data;
+using trackingg.Models;
+
+namespace trackingg.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IssueDbContext _context;
+        public IndexModel(IssueDbContext context) => _context = context;
+        public async void OnGet()
+        {
+            Issues = await _context.Issues.Where(i => i.Completed == null)
+                .OrderByDescending(i => i.Created)
+                .ToListAsync();
+        }
+
+        public IEnumerable<Issue> Issues { get; set; } = Enumerable.Empty<Issue>();
+    }
+}
